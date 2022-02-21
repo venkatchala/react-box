d836c165508924fa7bc6f5d68a908773e42757f1
venkat <venkatchala2@gmail.com>
2022-02-21 17:00:58 +0530

    Adding new project

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "blue", width: "400px", height: "400px" }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>Welcome!...</h1>
+      <h2 style={{ color: "white", textAlign: "center" }}>Venkat</h2>
+
     </div>
   );
 }
